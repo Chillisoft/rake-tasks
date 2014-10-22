d00b72c8f4d1ba74c15e16fc96bddcb15fe508ee
Kevin Bosman <kevin.bosman@chillisoft.co.za>
2014-10-22 14:07:08 +0200

    Added some visual feedback to installDependencies

--- installDependencies.cmd
+++ installDependencies.cmd
@@ -1,8 +1,16 @@
 @echo off
 setlocal
 cd %~dp0
+
+echo Installing Bundler...
 call gem install bundler
+
+echo Running Bundler install...
 call bundle install
+
+echo Installing Node.js modules
 call npm install
-copy Nuget.exe C:\Ruby193\bin
+
+echo.
+echo Done.
 pause
