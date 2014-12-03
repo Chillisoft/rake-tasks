0db96cc431f6d7c7c9ed3ea2760a7be708939109
mark.whitfeld <mark.whitfeld@chillisoft.co.za>
2014-12-03 13:57:53 +0200

    Improving the installDependencies.cmd to fix https bundler issue.

--- installDependencies.cmd
+++ installDependencies.cmd
@@ -5,6 +5,9 @@ cd %~dp0
 echo Installing Bundler...
 call gem install bundler
 
+echo Ensuring that gem Bundler will work properly with https
+call gem update --system
+
 echo Running Bundler install...
 call bundle install
 
